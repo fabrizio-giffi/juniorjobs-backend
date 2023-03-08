422ae82087050cac80cf655eb01f0c17eb49d9a4
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-03-08 12:48:40 +0100

    Fix a bug with update profile request

--- routes/api.user.routes.js
+++ routes/api.user.routes.js
@@ -5,7 +5,7 @@ const JobPost = require("../models/JobPost.model");
 router.get("/", async (req, res) => {
   try {
     const usersList = await User.find();
-    console.log(usersList)
+    console.log(usersList);
     res.json(usersList);
   } catch (err) {
     console.log(err);
@@ -33,9 +33,15 @@ router.get("/:id", async (req, res) => {
 });
 
 router.put("/edit/:id", async (req, res) => {
+  const changes = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    location: {
+      country: req.body.country,
+      city: req.body.city,
+    },
+  };
   try {
-    const changes = req.body;
-    delete changes.passwordHash;
     const currentUser = await User.findByIdAndUpdate(req.params.id, changes, {
       new: true,
     });
@@ -48,10 +54,10 @@ router.put("/edit/:id", async (req, res) => {
 });
 
 router.get("/publicprofile/:id", async (req, res) => {
-  console.log("PARAMS",req.params.id)
+  console.log("PARAMS", req.params.id);
   try {
     const currentUser = await User.findById(req.params.id);
-    console.log("PARAMS",req.params.id)
+    console.log("PARAMS", req.params.id);
     const responseUser = {
       firstName: currentUser.firstName,
       lastName: currentUser.lastName,
@@ -67,48 +73,47 @@ router.get("/publicprofile/:id", async (req, res) => {
 });
 
 router.put("/privateprofile/deleteFavJobPost", async (req, res) => {
-  const { id , postId } = req.body;
-  const currentUser = await User.findByIdAndUpdate(id, { $pull: { favoriteJobPosts: { $eq: postId } } }, {new : true} )
+  const { id, postId } = req.body;
+  const currentUser = await User.findByIdAndUpdate(id, { $pull: { favoriteJobPosts: { $eq: postId } } }, { new: true });
   // console.log(currentUser)
   res.status(200).json(currentUser);
-})
+});
 
 router.put("/privateprofile/deleteSkill", async (req, res) => {
-  const { id , skill } = req.body;
-  const currentUser = await User.findByIdAndUpdate(id, { $pull: { skills: { $eq: skill } } }, {new : true} )
+  const { id, skill } = req.body;
+  const currentUser = await User.findByIdAndUpdate(id, { $pull: { skills: { $eq: skill } } }, { new: true });
   // console.log(currentUser)
   res.status(200).json(currentUser);
-})
-
+});
 
 router.put("/privateprofile/deleteFavCompany", async (req, res) => {
-  const { id , companyId } = req.body;
-  const currentUser = await User.findByIdAndUpdate(id, { $pull: { favoriteCompanies: { $eq: companyId } } }, {new : true} )
+  const { id, companyId } = req.body;
+  const currentUser = await User.findByIdAndUpdate(
+    id,
+    { $pull: { favoriteCompanies: { $eq: companyId } } },
+    { new: true }
+  );
   // console.log(currentUser)
   res.status(200).json(currentUser);
-})
+});
 
 router.put("/addJobPost", async (req, res) => {
-  const { id , postId } = req.body;
+  const { id, postId } = req.body;
   const findJobPost = await JobPost.findById(postId);
-  const currentUser = await User.findByIdAndUpdate(id,
-     { $push: { favoriteJobPosts: findJobPost._id} }, {new : true} )
+  const currentUser = await User.findByIdAndUpdate(id, { $push: { favoriteJobPosts: findJobPost._id } }, { new: true });
   res.status(200).json(currentUser);
-})
+});
 
 router.put("/addCompany", async (req, res) => {
-  const { id , companyId } = req.body;
-  const currentUser = await User.findByIdAndUpdate(id,
-     { $push: { favoriteCompanies: companyId} }, {new : true} )
+  const { id, companyId } = req.body;
+  const currentUser = await User.findByIdAndUpdate(id, { $push: { favoriteCompanies: companyId } }, { new: true });
   res.status(200).json(currentUser);
-})
+});
 
 router.put("/addNewSkill", async (req, res) => {
-  const { id , newSkill } = req.body;
-  const currentUser = await User.findByIdAndUpdate(id,
-     { $push: { skills: newSkill} }, {new : true} )
+  const { id, newSkill } = req.body;
+  const currentUser = await User.findByIdAndUpdate(id, { $push: { skills: newSkill } }, { new: true });
   res.status(200).json(currentUser);
-})
-
+});
 
 module.exports = router;
