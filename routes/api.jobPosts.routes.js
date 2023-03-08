aa1e6ff30d9781f263465287ccb976ff19124c0c
DeanStavenuiter <ddstavenuiter@gmail.com>
2023-03-08 18:01:28 +0100

    no idea

--- routes/api.jobPosts.routes.js
+++ routes/api.jobPosts.routes.js
@@ -16,7 +16,9 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const currentJobPost = await JobPost.findById(req.params.id).populate("company");
+    const currentJobPost = await JobPost.findById(req.params.id).populate(
+      "company"
+    );
     res.json(currentJobPost);
   } catch (err) {
     console.log(err);
@@ -25,10 +27,23 @@ router.get("/:id", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { title, description, email, salaryRange, address, company, stack } = req.body;
-  const editJobBody = { title, description, email, salaryRange, address, company, stack };
+  const { title, description, email, salaryRange, address, company, stack } =
+    req.body;
+  const editJobBody = {
+    title,
+    description,
+    email,
+    salaryRange,
+    address,
+    company,
+    stack,
+  };
   try {
-    const editJobPost = await JobPost.findByIdAndUpdate(req.params.id, editJobBody, { new: true });
+    const editJobPost = await JobPost.findByIdAndUpdate(
+      req.params.id,
+      editJobBody,
+      { new: true }
+    );
     res.json(editJobPost);
   } catch (err) {
     console.log(err);
@@ -37,11 +52,24 @@ router.put("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { title, description, email, salaryRange, address, company, stack } = req.body;
-  const newJobBody = { title, description, email, salaryRange, address, company, stack };
+  const { title, description, email, salaryRange, address, company, stack } =
+    req.body;
+  const newJobBody = {
+    title,
+    description,
+    email,
+    salaryRange,
+    address,
+    company,
+    stack,
+  };
   try {
     const newJobPost = await JobPost.create(newJobBody);
-    await Company.findByIdAndUpdate(company, { $push: { jobPosts: newJobPost._id } }, { new: true });
+    await Company.findByIdAndUpdate(
+      company,
+      { $push: { jobPosts: newJobPost._id } },
+      { new: true }
+    );
     res.status(201).json({ id: newJobPost._id });
   } catch (err) {
     console.log(err);
@@ -53,20 +81,27 @@ router.put("/edit/:id", isAuthenticated, isJobPoster, async (req, res) => {
   try {
     const changes = req.body;
     delete changes.passwordHash;
-    const currentJobPost = await JobPost.findByIdAndUpdate(req.params.id, changes, { new: true });
-    console.log(req.payload);
+    const currentJobPost = await JobPost.findByIdAndUpdate(
+      req.params.id,
+      changes,
+      { new: true }
+    );
     res.json(currentJobPost);
-  } catch (err) {
-    console.log(err);
-    res.status(404).json({ message: err.message });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
   }
 });
 
 router.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
-  const deleteJobPost = await JobPost.findByIdAndDelete(id);
-  console.log("DELETE JOBSPOST", deleteJobPost);
-  res.status(200).json(deleteJobPost);
+  try {
+    const deleteJobPost = await JobPost.findByIdAndDelete(id);
+    res.status(200).json(deleteJobPost);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({message: error})
+  }
 });
 
 module.exports = router;
